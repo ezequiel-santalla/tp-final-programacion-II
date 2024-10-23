d1f1ffab12e70fd93cab58049a3edc27cda20f82
rodrilang <rodrigolang90mail.com>
2024-10-23 20:47:40 -0300

    persistence file

--- src/services/JSONConverter.java
+++ src/services/JSONConverter.java
@@ -1,7 +1,27 @@
 package services;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 public class JSONConverter {
-    ObjectMapper objectMapper = new ObjectMapper();
+    private static ObjectMapper objectMapper = new ObjectMapper();
+    public static <T> List<T> fromJsonArrayToList(String jsonString, Class<T> tClass) throws JsonProcessingException, IllegalArgumentException {
+        List<Map<String, Object>> mapList;
+        mapList = objectMapper.readValue(jsonString, List.class);
+        List<T> list = new ArrayList<>();
+
+        for (Map<String, Object> t : mapList) {
+            list.add(objectMapper.convertValue(t, tClass));
+        }
+        return list;
+    }
+
+
+    public static String toJson(Object object) throws JsonProcessingException {
+        return objectMapper.writeValueAsString(object);
+    }
 }
--- src/services/PersistenceFile.java
+++ src/services/PersistenceFile.java
@@ -1,6 +1,61 @@
 package services;
 
+import java.io.*;
+
 public class PersistenceFile {
 
+    public void createFile(String filePath) {
+        File file = new File(filePath);
+        try {
+            PrintWriter pw = new PrintWriter(file);
+            pw.close();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void writeFile(String filePath, String data) {
+        File file = new File(filePath);
+        try {
+            PrintWriter pw = new PrintWriter(file);
+            pw.println(data);
+            pw.close();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void addDataToFile(String filePath, String data) {
+        File file = new File(filePath);
+        try {
+            PrintWriter pw = new PrintWriter(new FileWriter(file, true));
+            pw.println(data);
+            pw.close();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String readFile(String filePath){
+        StringBuilder contentFile = new StringBuilder();
+        File file = new File(filePath);
+
+        try {
+            BufferedReader buff = new BufferedReader(new FileReader(file));
+            String currentLine = buff.readLine();
+            while(currentLine!=null){
+                contentFile.append(currentLine);
+                currentLine = buff.readLine();
+            }
+            buff.close();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return contentFile.toString();
+    }
 
 }
